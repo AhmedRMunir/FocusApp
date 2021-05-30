216c5b3907d42cb83048655920cce7cae4c12b5e
Ahmed <armunir@uw.edu>
2021-05-30 12:27:34 -0700

    Made App basics. Added a basic home screen

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.metrics import dp
+from kivy.properties import StringProperty, BooleanProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.label import Label
+from kivy.animation import Animation
+from kivy.properties import StringProperty, NumericProperty
+from kivy.uix.floatlayout import FloatLayout
 
-class TheLabApp(App):
+class FirstWindow(Screen):
     pass
 
-TheLabApp().run()
+class SecondWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class PhloApp(App):
+    pass
+
+PhloApp().run()
