b95bdd6384604f88a2a2e65a5762d5202038d7d8
Ahmed <armunir@uw.edu>
2021-05-31 16:36:46 -0700

    Added dark background to all screens. Added Buttons and fonts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
@@ -12,24 +13,45 @@ from kivy.uix.label import Label
 from kivy.animation import Animation
 from kivy.properties import StringProperty, NumericProperty
 from kivy.uix.floatlayout import FloatLayout
-
+from kivy.uix.settings import SettingsWithSidebar
+from kivy.lang import Builder
+from settingsjson import settings_json
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.image import Image
 
-class ImageButton(ButtonBehavior, Image):
+class HomePage(Screen):
     pass
 
-class FirstWindow(Screen):
-    pass
-
-class SecondWindow(Screen):
-    pass
+class WorkPage(Screen):
+    def show_details(self, widget):
+        print(widget)
 
 class WindowManager(ScreenManager):
     pass
 
-
+# Builder.load_file("phlo.kv")
 class PhloApp(App):
-    pass
+    def build(self):
+        self.settings_cls = SettingsWithSidebar
+        self.use_kivy_settings = False
+        setting = self.config.get('example', 'boolexample')
+        return WindowManager()
+    
+    def build_config(self, config):
+        config.setdefaults("example", {
+            "boolexample": True,
+            "numericexample": 10,
+            "optionsexample": "option2",
+            "stringexample": "string",
+            "pathexample": "/path"
+        })
+    
+    def build_settings(self, settings):
+        settings.add_json_panel("Panel Name", 
+                                 self.config,
+                                 data=settings_json)
+    
+    def on_config_change(self, config, section, key, value):
+        print(config, section, key, value)
 
 PhloApp().run()
